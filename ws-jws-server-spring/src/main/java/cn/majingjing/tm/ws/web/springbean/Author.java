bf85c085fb9befab5c9c1e64785fcaa3103b29e6
majingjing <majingjing@91xcm.com>
2018-06-07 16:05:14 +0800

    增加spring的bean注入到webservice服务中

---
+++ ws-jws-server-spring/src/main/java/cn/majingjing/tm/ws/web/springbean/Author.java
@@ -0,0 +1,12 @@
+package cn.majingjing.tm.ws.web.springbean;
+
+/**
+ * Created by JingjingMa on 2018/6/7 15:57
+ */
+public class Author {
+
+    public String name(){
+        return "皇太极";
+    }
+
+}
